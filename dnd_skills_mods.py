ec402e373838c886c8e5581a6e5d90519574c3b4
shivani <shivani@Shivanis-MBP-2.fios-router.home>
2020-05-04 16:50:33 -0400

    a lot of work on various ability scores

--- dnd_skills_mods.py
+++ dnd_skills_mods.py
@@ -13,8 +13,6 @@ for item in scores:
     mod = int((number - 10)/2)
     ability_mods.update({item:mod})
 
-print(ability_mods)
-
 #get list of all skills
 skills = requests.get("https://www.dnd5eapi.co/api/skills")
 skills_result = json.loads(skills.text)
@@ -36,4 +34,64 @@ for skill in skill_list:
     score_bonus = ability_mods[corresponding_score]
     bonus_list.update({skill:score_bonus})
 
-print(bonus_list)
+#add proficiencies
+pr = open("random_proficiencies.json")
+prof = json.load(pr)
+
+proficiency_bonus = 2
+
+for item in prof:
+    name = item['name'][7:]
+    for item in bonus_list:
+        if item == name:
+            bonus_list[item] = bonus_list[item] + proficiency_bonus
+
+rc = open("random_raceclass.json")
+raceclass = json.load(rc)
+
+x = requests.get(raceclass['race_url'])
+x_result = json.loads(x.text)
+speed = x_result['speed']
+
+save = requests.get(raceclass['class_url'])
+save_result = json.loads(save.text)
+saves = []
+for item in save_result['saving_throws']:
+    name = item['name'].lower()
+    saves.append(name)
+hp_base = save_result['hit_die']
+
+saving_throws = {}
+for item in ability_mods:
+    saving_throws[item] = ability_mods[item]
+
+for item in saving_throws:
+    if item in saves:
+        saving_throws[item] = saving_throws[item] + proficiency_bonus
+
+armor_class = 10+ability_mods['dex']
+initiative_bonus = ability_mods['dex']
+passive_perception = 10+ability_mods['wis']
+hit_points = hp_base+ability_mods['con']
+
+other_scores = {
+                "Armor Class":armor_class,
+                "Initiative":initiative_bonus,
+                "Passive Perception":passive_perception,
+                "Hit Points": hit_points,
+                "Hit Dice": "1d"+str(hp_base),
+                "Speed": speed,
+                "Proficiency Bonus": proficiency_bonus
+                }
+
+with open("ability_mods.json","w") as mods_file:
+    json.dump(ability_mods,mods_file)
+
+with open("saving_throws.json","w") as st_file:
+    json.dump(saving_throws,st_file)
+
+with open("skills.json","w") as skill_file:
+    json.dump(bonus_list,skill_file)
+
+with open("other_scores.json","w") as other_file:
+    json.dump(other_scores,other_file)
